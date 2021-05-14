fe2ecd3ebbab9a79d4d21ab8d313cc53b45fbe24
Willian Oliveira Siqueira Cavalcante <woscavalcante@outlook.com>
2021-05-13 23:49:45 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -1,25 +1,26 @@
 package com.devsuperior.dsclient.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = new ArrayList<>();
-		Instant birthDate = Instant.parse("1972-11-20T16:30:00Z");
-		list.add(new Client(1L, "João da Silva", "231.453.221-23", 4200.0, birthDate , 1));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
---
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dsclient.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository client;
+
+	public List<Client> findAll() {
+		return client.findAll();
+	}
+}
